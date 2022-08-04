3647c99e14f494bbcb68ee4dfd7018bb901b8211
Jeremy Guan <32561489+jeremyguan1@users.noreply.github.com>
2022-08-03 21:00:34 -0700

    Added Modal

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 (function(){
+
+    let test = document.getElementById('test')
+
+    test.addEventListener('click', () => {
+        console.log('here')
+        document.querySelector('.modalParent').style.display = 'block'
+    })
+
+    function handleClickFunction() {
+        console.log('hello')
+    }
     
+    function handleButtonClicked(e) {
+        console.log(e.target.id)
+    }
 })()
 
-function handleClickFunction() {
-    console.log('hello')
-}
-
-function handleButtonClicked(e) {
-    console.log(e.target.id)
-}
